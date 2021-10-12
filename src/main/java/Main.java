661490c5b6436f644444fa16bef1e34d8ed2287b
3ummerW1nd <sun_0901@foxmail.com>
2021-10-12 13:06:35 +0800

    ignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,6 @@ import javax.swing.*;
 
 public class Main {
   public static void main(String[] args) {
-    //        Room room = new Room();
-    //        room.initRoom();
-    //        room.askForGame();
     RoomWindow roomWindow = new RoomWindow();
     roomWindow.initRoom();
   }
